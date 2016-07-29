e61a070dc1925d06fdbb937b7aa23906abaf513e
alan echer <alan.echer@gmail.com>
2016-07-29 18:16:45 -0300

    commit

---
+++ web/js/theme.js
@@ -0,0 +1,173 @@
+/////////////////////////////////////////////////////////////////////
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+/////////////////////////////////////////////////////////////////////
+
+$('.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top -64
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+
+
+////////////////////////////////////////////////////////////////////////
+// On-Scroll Animated Header: https://github.com/codrops/AnimatedHeader
+////////////////////////////////////////////////////////////////////////
+
+var cbpAnimatedHeader = (function() {
+
+    var docElem = document.documentElement,
+        header = document.querySelector( '.navbar-fixed-top' ),
+        didScroll = false,
+        changeHeaderOn = 10;
+
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'navbar-shrink' );
+        }
+        else {
+            classie.remove( header, 'navbar-shrink' );
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
+
+
+
+//////////////////////////////////////////////
+// Highlight the top nav as scrolling occurs
+//////////////////////////////////////////////
+
+$('body').scrollspy({
+    target: '.navbar',
+    offset: 65
+})
+
+
+
+///////////////////////////////////////////
+// Display loading image while page loads
+///////////////////////////////////////////
+
+// Wait for window load
+$(window).load(function() {
+    // Animate loader off screen
+    $(".page-loader").fadeOut("slow");
+});
+
+////////////////////////////////////////////////////
+// OWL Carousel: http://owlgraphic.com/owlcarousel
+////////////////////////////////////////////////////
+
+// Intro text carousel
+$("#owl-intro-text").owlCarousel({
+    singleItem : true,
+    autoPlay : 6000,
+    stopOnHover : true,
+    navigation : false,
+    navigationText : false,
+    pagination : true
+})
+
+
+
+////////////////////////////////////////////////////////////////////
+// Stellar (parallax): https://github.com/markdalgleish/stellar.js
+////////////////////////////////////////////////////////////////////
+
+$.stellar({
+    // Set scrolling to be in either one or both directions
+    horizontalScrolling: false,
+    verticalScrolling: true,
+});
+
+
+
+///////////////////////////////////////////////////////////
+// WOW animation scroll: https://github.com/matthieua/WOW
+///////////////////////////////////////////////////////////
+
+new WOW().init();
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Isotop Package
+////////////////////////////////////////////////////////////////////////////////////////////
+$(window).load(function() {
+$('.portfolio_menu ul li').click(function(){
+	$('.portfolio_menu ul li').removeClass('active_prot_menu');
+	$(this).addClass('active_prot_menu');
+});
+
+var $container = $('#portfolio');
+$container.isotope({
+  itemSelector: '.col-sm-4',
+  layoutMode: 'fitRows'
+});
+$('#filters').on( 'click', 'a', function() {
+  var filterValue = $(this).attr('data-filter');
+  $container.isotope({ filter: filterValue });
+  return false;
+});
+});
+
+
+
+/////////////////////////
+// Scroll to top button
+/////////////////////////
+
+// Check to see if the window is top if not then display button
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+        $('.scrolltotop').fadeIn();
+    } else {
+        $('.scrolltotop').fadeOut();
+    }
+});
+
+// Click event to scroll to top
+$('.scrolltotop').click(function(){
+    $('html, body').animate({scrollTop : 0}, 1500, 'easeInOutExpo');
+    return false;
+});
+
+
+
+////////////////////////////////////////////////////////////////////
+// Close mobile menu when click menu link (Bootstrap default menu)
+////////////////////////////////////////////////////////////////////
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_579a8b18c0f4ac7924b25f37';
+(function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/579a8b18c0f4ac7924b25f37/1aopr14dg';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);})();
